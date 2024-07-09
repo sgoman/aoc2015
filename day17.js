88d4f8f01d517be104bf61354eb866c0b406cb04
Manuel Sgobino <msgobino@netcologne.de>
2024-07-09 15:19:35 +0200

    more examples for combineConditionally

--- day17.js
+++ day17.js
@@ -3,34 +3,42 @@
 const parseInput = input => input.split('\n').map(Number)
 
 /**
- * Creates combinations of the input elements and checks acceptCombo to keep a combiination
+ * Creates combinations of the input elements and checks acceptCombo to keep or discard a combination
  * and diveDeeper to decide if there could be acceptable combinations by continuing a depth search.
  *
  * Examples:
  *      const pairs = tmp => tmp.length == 2
- *   	const shorts = tmp => tmp.length < 2
+ *   	const tooShort = tmp => tmp.length < 2
+ *   	const onlyEven = tmp => tmp.every(val => val % 2 == 0)
  *   	const everything = tmp => true
  *
- *   	console.log(combineConditionally(['a', 'b', 'c'], pairs, shorts))
+ *   	console.log(combineConditionally(['a', 'b', 'c'], pairs, tooShort))
  *      // prints [ [ 'a', 'b' ], [ 'a', 'c' ], [ 'b', 'c' ] ]
+ *
+ *   	console.log(combineConditionally([1, 2, 3, 4, 5, 6], onlyEven, everything))
+ *      // prints [ [ 2 ], [ 2, 4 ], [ 2, 4, 6 ], [ 2, 6 ], [ 4 ], [ 4, 6 ], [ 6 ] ]
  *
  *   	console.log(combineConditionally(['a', 'b', 'c'], everything, everything))
  *   	// prints [ [ 'a' ], [ 'a', 'b' ], [ 'a', 'b', 'c' ], [ 'a', 'c' ], [ 'b' ], [ 'b', 'c' ], [ 'c' ] ]
  *
+ * @param array arr the array to source combinations from
+ * @param function acceptCombo takes the current combination, returns boolean to keep or discard
+ * @param function diveDeeper takes the current combination, returns boolean to keep adding or stopping
+ * @returns array of arrays of possible combinations
  */ 
 const combineConditionally = (arr, acceptCombo, diveDeeper) => {
 	const l = arr.length
-	const c = (part, start) => {
+	const combiner = (part, start) => {
 		let result = [], p
 		for (let i = start; i < l; i++) {
 			p = part.slice(0)
 			p.push(arr[i])
 			if (acceptCombo(p)) result.push(p)
-			if (diveDeeper(p)) result = result.concat(c(p, i + 1))
+			if (diveDeeper(p)) result = result.concat(combiner(p, i + 1))
 		}
 		return result
 	}
-	return c([], 0)
+	return combiner([], 0)
 }
 
 const arraySum = arr => arr.reduce((acc, cur) => acc + cur, 0)
